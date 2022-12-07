371d383072f47c82b4331da037406d2c80541e9d
Liudmyla007 <ldatsenko3@gmail.com>
2022-12-07 22:50:16 +0200

    finished 1task

--- js/task-01.js
+++ js/task-01.js
@@ -1,39 +1,6 @@
-const list = document.querySelector("#categories");
-const title = document.createElement("p");
-const itemsQuant = document.getElementsByClassName('item');
-title.textContent = `Number of categories: ${itemsQuant.length}`;
-list.before(title);
-
-const myList = document.createElement("ul");
-title.after(myList);
-
-const firstLi = document.createElement("li");
-const preTitle = document.querySelector("h2");
-firstLi.textContent = `Category: ${preTitle.textContent}`;
-myList.prepend(firstLi);
-
-const secondLi = document.createElement("li");
-
-const b = document.querySelector(".item");
-const c = b.lastElementChild;
-console.log(c);
-const d = c.children;
-console.log(d.length);
-
-secondLi.textContent = `Elements: ${d.length}`;
-myList.append(secondLi);
-
-//
-//  const b = document.querySelector(".item");
-// const c = b.lastElementChild;
-// console.log(c);
-// const d = c.children;
-// console.log(d.length);
-
-
-
-
-//  const preItemQuant = d.length;
-
-
-
+const myList = document.querySelectorAll('.item');
+console.log(`Number of categories: ${myList.length}`);
+myList.forEach((elem) => {
+    console.log(`Category: ${elem.firstElementChild.textContent}`);
+    console.log(`Elements: ${elem.lastElementChild.children.length}`);
+});
