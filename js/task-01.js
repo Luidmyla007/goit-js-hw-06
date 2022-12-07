32d5e31ce0277feacee3af977bb84da00e0365b4
Liudmyla007 <ldatsenko3@gmail.com>
2022-12-07 15:46:43 +0200

    first_part

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,39 @@
 const list = document.querySelector("#categories");
-const title = document.createElement("h1");
+const title = document.createElement("p");
 const itemsQuant = document.getElementsByClassName('item');
 title.textContent = `Number of categories: ${itemsQuant.length}`;
+list.before(title);
 
- list.before(title);
-// const myList = document.createElement("ul");
-// title.after(myList);
+const myList = document.createElement("ul");
+title.after(myList);
+
+const firstLi = document.createElement("li");
+const preTitle = document.querySelector("h2");
+firstLi.textContent = `Category: ${preTitle.textContent}`;
+myList.prepend(firstLi);
+
+const secondLi = document.createElement("li");
+
+const b = document.querySelector(".item");
+const c = b.lastElementChild;
+console.log(c);
+const d = c.children;
+console.log(d.length);
+
+secondLi.textContent = `Elements: ${d.length}`;
+myList.append(secondLi);
+
+//
+//  const b = document.querySelector(".item");
+// const c = b.lastElementChild;
+// console.log(c);
+// const d = c.children;
+// console.log(d.length);
+
+
+
+
+//  const preItemQuant = d.length;
+
 
 
