a42b653c731dd6f99e808ade397d65b509a8fd95
Liudmyla007 <ldatsenko3@gmail.com>
2022-12-08 17:43:14 +0200

    task4 done

--- js/task-04.js
+++ js/task-04.js
@@ -1,2 +1,17 @@
-const myCounter = document.querySelector("#counter");
-// console.log(myCounter);
+const btnDecr = document.querySelector('button[data-action="decrement"]');
+const btnIncr = document.querySelector('button[data-action="increment"]');
+
+let counterValue = 0;
+
+btnDecr.addEventListener("click", () => {
+    console.log(counterValue -= 1);     
+});
+
+btnIncr.addEventListener("click", () => {
+    console.log(counterValue += 1);
+});
+
+
+
+
+
