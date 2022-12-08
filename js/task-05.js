e631a1cf45fbec0d19b5ce72ef913f2896666237
Liudmyla007 <ldatsenko3@gmail.com>
2022-12-08 22:28:53 +0200

    update task5

--- js/task-05.js
+++ js/task-05.js
@@ -1,9 +1,13 @@
 const textOfInput = document.querySelector("#name-input");
 const textOfSpan = document.querySelector("#name-output");
 
-textOfInput.addEventListener("input", (event) => {
-    textOfSpan.textContent = event.currentTarget.value;
-});
+textOfInput.addEventListener("input", () => 
+    textOfInput.value
+        ? (textOfSpan.textContent = textOfInput.value)
+        : (textOfSpan.textContent = "Anonymous")
+);
+
+
 
 
 
