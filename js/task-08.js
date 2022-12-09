2c588b9cda1d8ad513d9dfaf66abb4ca070b3caa
Liudmyla007 <ldatsenko3@gmail.com>
2022-12-09 18:55:59 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const myForm = document.querySelector(".login-form");
+console.log(myForm);
+
+myForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { login, password }
+    } = event.currentTarget;
+
+    if (myForm.elements.email.value === "" || myForm.elements.password.value === "") {
+        return alert("Please fill in all the fields!");
+    }
+
+    const resume = {
+        email:  myForm.elements.email.value,
+        password: myForm.elements.password.value};
+    console.log(resume);
+  
+  event.currentTarget.reset();
+}
 
