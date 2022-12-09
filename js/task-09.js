3d3672d0ed6daf950116332ed1c8ba82f87a064e
Liudmyla007 <ldatsenko3@gmail.com>
2022-12-09 20:32:43 +0200

    10 update

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,23 @@
+const magicBtn = document.querySelector(".change-color");
+const nameColor = document.querySelector(".color");
+const chameleonBody = document.querySelector("body");
+
+
+magicBtn.addEventListener("click", getRandomHexColor); 
+ 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+  let newColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  chameleonBody.style.backgroundColor = newColor;
+  nameColor.textContent = newColor;
+};
+
+
+
+
+
+
+
+// це було  в умові
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
