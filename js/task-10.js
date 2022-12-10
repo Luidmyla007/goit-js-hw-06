9cbf66d53b35ddb943aeed9823076c5c23057261
Liudmyla007 <ldatsenko3@gmail.com>
2022-12-10 22:02:07 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,28 +1,7 @@
-const myInput = document.querySelector("input");
-const btnCreate = document.querySelector('button[data-create]');
-const btnDestroy = document.querySelector('button[data-destroy]');
-const ownerBox = document.querySelector("#boxes");
-
- function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
- }
-
-myInput.addEventListener("input", (event) => {
-  event.currentTarget.value = amount;
-});
-
-function createBoxes(amount) {
-  myInput.addEventListener("input", (event) => {
-  event.currentTarget.value = amount;
-});
-  let newDives = [];
-  for (let i = 0; i <= amount; i++)
-  newDiv = document.createElement("div");
-  newDives.push(newDiv);
-  ownerBox.append(newDives);
 
 
-};
+
+  
 
 
 
